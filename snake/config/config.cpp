49cd154debe9e6177e0987c5381153158c2c8d30
shaguargeffon <shaguarhan123@gmail.com>
2022-09-23 00:56:49 +0200

    modify-const-to-constexpr

--- snake/config/config.cpp
+++ snake/config/config.cpp
@@ -1,30 +1,30 @@
 #include "config.h"
 
 
-const unsigned int timer_scale{20};
+constexpr unsigned int timer_scale{20};
 
-const unsigned int timer_multiplier_hard{5};
-const unsigned int timer_multiplier_normal{10};
-const unsigned int timer_multiplier_easy{20};
+constexpr unsigned int timer_multiplier_hard{5};
+constexpr unsigned int timer_multiplier_normal{10};
+constexpr unsigned int timer_multiplier_easy{20};
 
-const unsigned int timer_creation_new_block{timer_multiplier_easy * 10};
+constexpr unsigned int timer_creation_new_block{timer_multiplier_easy * 10};
 
-const unsigned int game_size_x{2000};
-const unsigned int game_size_y{1500};
+constexpr unsigned int game_size_x{2000};
+constexpr unsigned int game_size_y{1500};
 
-const unsigned int frame_scope_x{1500};
-const unsigned int frame_scope_y{1500};
+constexpr unsigned int frame_scope_x{1500};
+constexpr unsigned int frame_scope_y{1500};
 
-const unsigned int frame_amount_x{30};
-const unsigned int frame_amount_y{30};
+constexpr unsigned int frame_amount_x{30};
+constexpr unsigned int frame_amount_y{30};
 
-const unsigned int element_size_x{40};
-const unsigned int element_size_y{40};
+constexpr unsigned int element_size_x{40};
+constexpr unsigned int element_size_y{40};
 
-const unsigned int start_point_coordinate_x{100};
-const unsigned int start_point_coordinate_y{100};
+constexpr unsigned int start_point_coordinate_x{100};
+constexpr unsigned int start_point_coordinate_y{100};
 
-const unsigned int first_snake_x{frame_amount_x/2};
-const unsigned int first_snake_y{frame_amount_y/2};
+constexpr unsigned int first_snake_x{frame_amount_x/2};
+constexpr unsigned int first_snake_y{frame_amount_y/2};
 
-extern const unsigned int game_snake_max_length{frame_amount_x / 3};
+constexpr unsigned int game_snake_max_length{frame_amount_x / 3};
